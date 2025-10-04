6e33bb842ac590a7e93c27dafa8d28bedccc71bf
Daniel-1-2-3 <144050857+Daniel-1-2-3@users.noreply.github.com>
2025-10-03 21:56:51 -0400

    update jump

--- manual.py
+++ manual.py
@@ -6,34 +6,35 @@ import os
 import pickle
 import neat
 
-def loadNeuralNetwork():
-    local_dir = os.path.dirname(__file__) #gets the directory that we are currently in
-    config_file = os.path.join(local_dir, "config.txt") #find the path to the config.txt file
-    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet,
-                                neat.DefaultStagnation, config_file)
-    with open('winning_genome.pkl', 'rb') as f:
-        perfect_genome = pickle.load(f)
-    neural_network = neat.nn.FeedForwardNetwork.create(perfect_genome, config)
-    return neural_network
-
 pygame.init()
 game = Game(velocity=500)
-network = loadNeuralNetwork()
 game.reset_game(n=1)
 
 run = True
-while run:
-    dino = game.dinos[0]
+play = True
+while play:
+    while run:
+        dino = game.dinos[0]
+        
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT:
+                run = False
+                play = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE or pygame.K_UP and dino.jumps_used < 2: 
+                    dino.jump()
+        
+        game.step()
+        if not dino.alive:
+            run = False
+            break
     
+    time.sleep(0.5)
     for event in pygame.event.get(): 
         if event.type == pygame.QUIT:
             run = False
-            game = False
+            play = False
         if event.type == pygame.KEYDOWN:
+            game.reset_game(n=1)
             if event.key == pygame.K_SPACE or pygame.K_UP: 
-                startGame = True
-                dino.jump()
-    
-    game.step()
-    if not dino.alive:
-        break
+                run = True
